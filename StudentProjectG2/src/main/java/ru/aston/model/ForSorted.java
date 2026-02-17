c58b3d4d6ea516a1ccc62035482cbe616a4b72a1
Иван Котеля <kotelya496@gmail.com>
2026-02-17 19:09:33 +0600

    Добавление кода

---
+++ StudentProjectG2/src/main/java/ru/aston/model/ForSorted.java
@@ -0,0 +1,36 @@
+package ru.aston.model;
+
+import java.util.Comparator;
+
+public enum ForSorted implements Comparator<Student> {
+    numberOfGroup{
+        @Override
+        public int compare(Student student1, Student student2) {
+            if (student1.getNumberOfGroup()<student2.getNumberOfGroup())
+                return -1;
+            if (student1.getNumberOfGroup()>student2.getNumberOfGroup())
+                return 1;
+            return 0;
+        }
+    },
+    averageGrade {
+        @Override
+        public int compare(Student student1, Student student2) {
+            if (student1.getAverageGrade()<student2.getAverageGrade())
+                return -1;
+            if (student1.getAverageGrade()>student2.getAverageGrade())
+                return 1;
+            return 0;
+        }
+    },
+    numberOfRecordBook {
+        @Override
+        public int compare(Student student1, Student student2) {
+            if (student1.getNumberOfRecordBook()<student2.getNumberOfRecordBook())
+                return -1;
+            if (student1.getNumberOfRecordBook()>student2.getNumberOfRecordBook())
+                return 1;
+            return 0;
+        }
+    };
+}
--- StudentProjectG2/src/main/java/ru/aston/model/Student.java
+++ StudentProjectG2/src/main/java/ru/aston/model/Student.java
@@ -1,4 +1,62 @@
 package ru.aston.model;
 
 public class Student {
+    private int numberOfGroup;
+    private int averageGrade;
+    private int numberOfRecordBook;
+
+    public Student(int numberOfGroup, int averageGrade, int numberOfRecordBook) {
+        this.numberOfGroup = numberOfGroup;
+        this.averageGrade = averageGrade;
+        this.numberOfRecordBook = numberOfRecordBook;
+    }
+
+    public int getNumberOfGroup() {
+        return numberOfGroup;
+    }
+
+    public int getAverageGrade() {
+        return averageGrade;
+    }
+
+    public int getNumberOfRecordBook() {
+        return numberOfRecordBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "numberOfGroup=" + numberOfGroup +
+                ", averageGrade=" + averageGrade +
+                ", numberOfRecordBook=" + numberOfRecordBook +
+                '}';
+    }
+
+    public static BuilderStudent builderStudent(){
+        return new BuilderStudent();
+    }
+
+    public static class BuilderStudent {
+        private int numberOfGroup;
+        private int averageGrade;
+        private int numberOfRecordBook;
+
+        public BuilderStudent setNumberOfGroup(int numberOfGroup) {
+            this.numberOfGroup = numberOfGroup;
+            return this;
+        }
+
+        public BuilderStudent setAverageGrade(int averageGrade) {
+            this.averageGrade = averageGrade;
+            return this;
+        }
+
+        public BuilderStudent setNumberOfRecordBook(int numberOfRecordBook) {
+            this.numberOfRecordBook = numberOfRecordBook;
+            return this;
+        }
+        public Student build(){
+            return new Student(numberOfGroup,averageGrade,numberOfRecordBook);
+        }
+    }
 }
