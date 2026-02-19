72605e8b35c559a260b3fe656cc6042fcf987606
Your Name <you@example.com>
2026-02-19 21:35:35 +0500

    зачетная книжка

--- StudentProjectG2/src/main/java/ru/aston/validation/StudentValidator.java
+++ StudentProjectG2/src/main/java/ru/aston/validation/StudentValidator.java
@@ -2,18 +2,22 @@ package ru.aston.validation;
 
 public class StudentValidator {
 
+    //валидация номера группы
     public static boolean validateGroup(int group){
         return group > 0 && group < 20;
     }
 
+    //валидация среднего балла
     public static boolean validateAverage(int avg){
-        return avg >= 0 && avg <=5;
+        return avg >= 1 && avg <=5;
     }
 
+    //валидация номера зачетной книжки
     public static boolean validateRecordBook(int record){
-        return record > 0;
+        return record >= 1000000 && record <= 9999999;
     }
 
+    //общий метод валидации для удобства вызова в стратегиях
     public static boolean isValid(int group, int avg, int record) {
         return validateGroup(group) && validateAverage(avg) && validateRecordBook(record);
     }
