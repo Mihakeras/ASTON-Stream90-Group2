ab5569298b90cd44064bc49c77e32426defef815
Your Name <you@example.com>
2026-02-19 13:52:27 +0500

    сделала validator

--- StudentProjectG2/src/main/java/ru/aston/validation/StudentValidator.java
+++ StudentProjectG2/src/main/java/ru/aston/validation/StudentValidator.java
@@ -1,4 +1,20 @@
 package ru.aston.validation;
 
 public class StudentValidator {
+
+    public static boolean validateGroup(int group){
+        return group > 0 && group < 20;
+    }
+
+    public static boolean validateAverage(int avg){
+        return avg >= 0 && avg <=5;
+    }
+
+    public static boolean validateRecordBook(int record){
+        return record > 0;
+    }
+
+    public static boolean isValid(int group, int avg, int record) {
+        return validateGroup(group) && validateAverage(avg) && validateRecordBook(record);
+    }
 }
