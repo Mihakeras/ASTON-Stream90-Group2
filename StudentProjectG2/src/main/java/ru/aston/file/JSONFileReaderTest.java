0835d17fada88ff6b5a0f9d872ca2b7be6e00a61
DANIILKONEF <daniilkonef@yandex.ru>
2026-02-20 17:46:40 +0300

    я добавил вывод содержимого json файла в консоль для JSONFileReaderTest
    Signed-off-by: DANIILKONEF <daniilkonef@yandex.ru>

---
+++ StudentProjectG2/src/main/java/ru/aston/file/JSONFileReaderTest.java
@@ -0,0 +1,22 @@
+package ru.aston.file;
+import java.io.IOException;
+
+public class JSONFileReaderTest {
+    static void main() {
+        //JSONFileReader fileReader = new JSONFileReader(); так нельзя делать, тк методы статичные внутри
+        boolean exists = JSONFileReader.checkThatFileExists("students.json");
+        boolean isJSON = JSONFileReader.checkIsJSONFile("students.json");
+        String contentOfFileAsOneString;
+        try {
+            contentOfFileAsOneString = JSONFileReader.readFileAsOneString("students.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("checkThatFileExists -> " + exists);
+        System.out.println("checkIsJSONFile -> " + isJSON);
+        System.out.println(contentOfFileAsOneString);
+
+    }
+}
+
